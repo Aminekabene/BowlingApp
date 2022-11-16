166c9d682cedb2d50c73c2e7ec33767a38ac85cb
Aminekabene <76407228+Aminekabene@users.noreply.github.com>
2022-11-16 08:20:45 -0500

    adding some comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        
+        // Here is a simulation of a bowling game with a perfect score.
         Game game = new Game();
         game.roll(10);
         game.score();
@@ -25,7 +26,6 @@ public class Main {
         game.roll(10);
         game.score();
         game.roll(10);
-
         game.score();
 
     }
