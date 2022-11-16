b5ea6eaf68d2bf55a8f87d75de40d9652f81ed89
Aminekabene <76407228+Aminekabene@users.noreply.github.com>
2022-11-15 22:40:13 -0500

    Creating scroll method in Game class

---
+++ src/Game.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Game {
+
+    // The index i in this linkedList represent the frame number and the nested Hash map in it stores the roll number as the key and the score of that roll as the value.
+    //        Frame            roll     scoreOfRoll
+    public LinkedList<HashMap<Integer, Integer>> board = new LinkedList<>();
+    private boolean isGameOver = false;
+    private int totalScore = 0;
+
+
+    public void roll(int pins){
+
+        if(pins > 10){
+            throw new RuntimeException("Error you can not knock down more than 10 pins.");
+        }
+
+        if(isGameOver){
+            throw new RuntimeException("Can not roll anymore the Game is Over.");
+        }
+
+        // This is the First roll
+        if (board.isEmpty()){
+            board.add(new HashMap<Integer,Integer>(){ {put(1,pins);} });
+            return;
+        }
+
+        int currentframe = board.size()-1;
+        if(board.get(currentframe).size() == 1 && board.get(currentframe).get(1) != 10){
+
+            if (pins + board.get(currentframe).get(1) > 10){
+                throw new RuntimeException("Error you can not knock down "+pins+" pins when there are only "+ (10-board.get(currentframe).get(1))+" pins remaining.");
+            }
+
+            board.get(currentframe).put(2,pins);
+            return;
+        }
+        else{
+            board.add(new HashMap<Integer,Integer>(){ {put(1,pins);} });
+        }
+
+
+    }
+
+    public void score(){
+
+    }
+
+    /* Helper Methods */
+
+    private int computeFrameScore(HashMap<Integer,Integer> map){
+        int sum =0;
+        for (int rollScore : map.values()){
+            sum += rollScore;
+        }
+        return sum;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Game game = new Game();
+        game.roll(8);
+        game.roll(2);
+        game.roll(10);
+        game.roll(4);
+        game.roll(4);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(2);
+        game.roll(3);
+
+        System.out.println(game.board.toString());
     }
 }
