78c5dc33f1f63452c8c587562b5360a771f0da50
Aminekabene <76407228+Aminekabene@users.noreply.github.com>
2022-11-16 01:44:33 -0500

    The Bowling game simulator is Working!

--- src/Game.java
+++ src/Game.java
@@ -4,9 +4,8 @@ public class Game {
 
     // The index i in this linkedList represent the frame number and the nested Hash map in it stores the roll number as the key and the score of that roll as the value.
     //        Frame            roll     scoreOfRoll
-    public LinkedList<HashMap<Integer, Integer>> board = new LinkedList<>();
+    private LinkedList<HashMap<Integer, Integer>> board = new LinkedList<>();
     private boolean isGameOver = false;
-    private int totalScore = 0;
 
 
     public void roll(int pins){
@@ -22,6 +21,7 @@ public class Game {
         // This is the First roll
         if (board.isEmpty()){
             board.add(new HashMap<Integer,Integer>(){ {put(1,pins);} });
+            System.out.println(board.toString());
             return;
         }
 
@@ -38,36 +38,77 @@ public class Game {
                 isGameOver = true;
                 System.out.println("Game Over !");
             }
-
+            System.out.println(board.toString());
             return;
+        }
+        else if(board.size() == 11 && board.get(currentframe).size() == 1){
+            board.get(currentframe).put(2,pins);
+            isGameOver = true;
+            System.out.println("Game Over !");
         }
         else{
             board.add(new HashMap<Integer,Integer>(){ {put(1,pins);} });
-            
-            if (board.size()==11){
-                int previousFrame = board.size()-2;
-                if(board.get(previousFrame).size()==2){
-                    isGameOver = true;
-                    System.out.println("Game Over !");
+        }
+        System.out.println(board.toString());
+    }
+
+    //This method compute the score for all rolls performed
+    public void score(){
+        int totalScore = 0;
+
+        if (board.isEmpty()){
+            throw new RuntimeException("Can not calculate the score, game has not started yet.");
+        }
+
+        for (int i =0; i < board.size(); i++){
+
+            if (i > 0){
+                int previousFrame = i-1;
+
+                // check if previous frame was a stike
+                if(board.get(previousFrame).size() == 1){
+                    totalScore += findNextTwoRolls(i);
                 }
+                // check if previous frame was a spare
+                else if(board.get(previousFrame).size() == 2 && computeFrameScore(board.get(previousFrame)) == 10){
+                    totalScore += board.get(i).get(1);
+                }
+            }
+
+            if(i+1 != 11){
+                totalScore += computeFrameScore(board.get(i));
             }
         }
 
-
-    }
-
-    public void score(){
-
+        System.out.println("The Score is: " + totalScore);
     }
 
     /* Helper Methods */
 
     private int computeFrameScore(HashMap<Integer,Integer> map){
-        int sum =0;
+        int sum = 0;
         for (int rollScore : map.values()){
             sum += rollScore;
         }
         return sum;
+    }
+
+    private int findNextTwoRolls(int index){
+
+        if (board.get(index).size() == 2)
+            return computeFrameScore(board.get(index));
+
+        int sum = 0;
+        int count = 0;
+        for(int i = index; i < board.size(); i++){
+            if (count == 2) {
+                break;
+            }
+            sum += board.get(i).get(1);
+            count++;
+        }
+
+        return  sum;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,31 @@ public class Main {
     public static void main(String[] args) {
 
         Game game = new Game();
-        game.roll(8);
-        game.roll(2);
         game.roll(10);
-        game.roll(4);
-        game.roll(4);
+        game.score();
         game.roll(10);
+        game.score();
         game.roll(10);
+        game.score();
         game.roll(10);
+        game.score();
         game.roll(10);
+        game.score();
         game.roll(10);
+        game.score();
         game.roll(10);
-        game.roll(8);
-        game.roll(2);
-        game.roll(2);
+        game.score();
+        game.roll(10);
+        game.score();
+        game.roll(10);
+        game.score();
+        game.roll(10);
+        game.score();
+        game.roll(10);
+        game.score();
+        game.roll(10);
 
-        System.out.println(game.board.toString());
+        game.score();
+
     }
 }
